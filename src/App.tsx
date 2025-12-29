363b7543d78961aff9a2a252a52dca822d1f0e74
WebTeam14 <web.technoriya@gmail.com>
2025-12-29 15:42:39 +0530

    Fix BrowserRouter basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import AboutUs from "./pages/AboutUs";
@@ -23,23 +23,20 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/our-team" element={<OurTeam />} />
-          <Route path="/mission-vision" element={<MissionVision />} />
-          <Route path="/design-services" element={<DesignServices />} />
-          <Route path="/consulting" element={<Consulting />} />
-          <Route path="/project-management" element={<ProjectManagement />} />
-          <Route path="/skill-development" element={<SkillDevelopment />} />
-          <Route path="/events" element={<Event />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/our-team" element={<OurTeam />} />
+        <Route path="/mission-vision" element={<MissionVision />} />
+        <Route path="/design-services" element={<DesignServices />} />
+        <Route path="/consulting" element={<Consulting />} />
+        <Route path="/project-management" element={<ProjectManagement />} />
+        <Route path="/skill-development" element={<SkillDevelopment />} />
+        <Route path="/events" element={<Event />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
